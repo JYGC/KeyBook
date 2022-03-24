9ea7f387a086ab4ab1e5c3141305df356e110901
Junying Chen <jygchen@gmail.com>
2022-03-24 15:06:53 +1100

    Remove compiled js files.

--- KeyBook/Svelte/src/main.js
+++
@@ -1,5 +0,0 @@
-import './components/App.svelte';
-import './components/Clapp.svelte';
-import './components/DeviceList.svelte';
-import './components/DeviceEdit.svelte';
-//# sourceMappingURL=main.js.map
