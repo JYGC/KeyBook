23446b0ce6888b040a983db5db9391195d694348
Junying Chen <jygchen@gmail.com>
2022-07-12 17:06:37 +1000

    Redirect from Login and Register if authenticated.

--- KeyBook/Program.cs
+++ KeyBook/Program.cs
@@ -65,7 +65,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Device}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
