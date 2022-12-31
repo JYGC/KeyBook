11e2d86f7fec62b9ac49c67812a2bfe542177e2d
Junying Chen <jygchen@gmail.com>
2023-01-01 10:56:37 +1100

    fix some Admin UI components

--- KeyBook/Services/UserRoleService.cs
+++ KeyBook/Services/UserRoleService.cs
@@ -10,12 +10,14 @@ namespace KeyBook.Services
         private readonly SignInManager<User> __signInManager;
         private readonly UserManager<User> __userManager;
         private readonly RoleManager<IdentityRole> __roleManager;
+        private readonly IHttpContextAccessor __httpContextAccessor;
 
-        public UserRoleService(SignInManager<User> signInManager, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        public UserRoleService(SignInManager<User> signInManager, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IHttpContextAccessor httpContextAccessor)
         {
             __signInManager = signInManager;
             __userManager = userManager;
             __roleManager = roleManager;
+            __httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<List<UserRolesViewModel>?> GetUserRolesByUserId(string userId)
@@ -32,6 +34,49 @@ namespace KeyBook.Services
                 userRolesViewModels.Add(userRolesViewModel);
             }
             return userRolesViewModels;
+        }
+
+        public async Task<(bool, string?)> ManageUserRolesForUser(string id, ManageUserRolesViewModel model)
+        {
+            try
+            {
+                User user = await __userManager.FindByIdAsync(id);
+                IList<string> roles = await __userManager.GetRolesAsync(user);
+                IdentityResult result = await __userManager.RemoveFromRolesAsync(user, roles);
+                if (__httpContextAccessor.HttpContext == null) throw new Exception("Cannot find HTTP context");
+                User currentUser = await __userManager.GetUserAsync(__httpContextAccessor.HttpContext.User);
+                await __signInManager.RefreshSignInAsync(currentUser);
+                await Seeds.DefaultUsers.SeedSuperAdminAsync(__userManager, __roleManager); // fallback code in case one admin tries to change the roles of another - Find better implementation?
+                return (true, null);
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+
+        public async Task<(bool, string?)> UpdateUserRolesForUser(string id, ManageUserRolesViewModel model)
+        {
+            try
+            {
+                User user = await __userManager.FindByIdAsync(id);
+                foreach (UserRolesViewModel userRolesViewModel in model.UserRoles)
+                {
+                    if (userRolesViewModel.Selected)
+                    {
+                        await __userManager.AddToRoleAsync(user, userRolesViewModel.RoleName);
+                    }
+                    else
+                    {
+                        await __userManager.RemoveFromRoleAsync(user, userRolesViewModel.RoleName);
+                    }
+                }
+                return (true, null);
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
         }
     }
 }
