61a46b196838fee7ef9fa9231e0d59bb2c20c1b5
Junying Chen <jygchen@gmail.com>
2023-05-20 22:25:20 +1000

    29 remove remains of mvc files (#34)
    * remove MVC controllers and views
    
    * Remove unused view models
    
    * remove ViewStart and MVC builder services

--- KeyBook/ViewModels/SavePersonDeviceRequestModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KeyBook.ViewModels
-{
-    public class SavePersonDeviceRequestModel
-    {
-        public Guid DeviceId { get; set; }
-        public Guid PersonId { get; set; }
-    }
-}
