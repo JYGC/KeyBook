77f6cdacbe26ffbdb75fa44bd3252018c0797884
Junying Chen <jygchen@gmail.com>
2025-10-18 10:24:31 +1100

    test app layout

--- frontend/src/routes/(app)/+layout.ts
+++ frontend/src/routes/(app)/+layout.ts
@@ -4,6 +4,7 @@ import { BackendClient } from "$lib/api/backend-client";
 
 export const load: LayoutLoad = async () => {
   const authManager = new BackendClient();
+  console.log(authManager.isTokenValid);
   if (!authManager.isTokenValid) {
     return redirect(303, "/auth");
   }
