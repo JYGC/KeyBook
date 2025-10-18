f3dc4e8879a81779a286b61845997dc4eb531de2
Junying Chen <jygchen@gmail.com>
2025-10-18 13:00:06 +1100

    unsecure cookie

--- frontend/src/lib/api/backend-client.ts
+++ frontend/src/lib/api/backend-client.ts
@@ -1,6 +1,6 @@
 import PocketBase, { type AuthModel } from "pocketbase";
 import type { IBackendClient } from "$lib/interfaces";
-import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+import { PUBLIC_POCKETBASE_URL, PUBLIC_UNSECURE_COOKIE } from '$env/static/public';
 
 export class BackendClient implements IBackendClient {
   private readonly __pb = new PocketBase();
@@ -9,7 +9,12 @@ export class BackendClient implements IBackendClient {
     this.__pb = new PocketBase(PUBLIC_POCKETBASE_URL);
     this.__pb.authStore.loadFromCookie(document.cookie);
     this.__pb.authStore.onChange(() => {
-      document.cookie = this.__pb.authStore.exportToCookie({ httpOnly: false });
+      document.cookie = this.__pb.authStore.exportToCookie({
+        httpOnly: false,
+        secure: PUBLIC_UNSECURE_COOKIE !== undefined &&
+          PUBLIC_UNSECURE_COOKIE !== null &&
+          PUBLIC_UNSECURE_COOKIE === 'true'
+      });
     });
   }
 
